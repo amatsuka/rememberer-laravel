98a4693ba5e198ce88890061a4d5d694cf3d8a59
amatsuka <run100500@gmail.com>
2019-11-04 17:39:00 +0300

    Сервис сохранения записей

--- app/Services/NoteService.php
+++ app/Services/NoteService.php
@@ -16,15 +16,25 @@ class NoteService {
     }
 
     public function store(array $attributes) : Note {
+        $note = new Note($attributes);
 
+        if ($attributes['password']) {
+            $note = $this->encryptNote($note, $attributes['password']);
+        }
+
+        return $note;
     }
 
     public function findByCode(string $code) : ?Note
-    { }
+    {
+        return Note::whereCode($code)->wherePasswordHash(null);
+    }
 
     public function findByCodeAndPassword(string $code, string $password) : ?Note
     {
+        $note = Note::whereCode($code)->wherePasswordHash(md5(password));
 
+        return $this->decryptNote($note, $password);
     }
 
     private function encryptNote(Note $note, string $password) : Note {
@@ -32,5 +42,7 @@ class NoteService {
     }
 
     private function decryptNote(Note $note, string $password) : Note
-    { }
+    {
+
+    }
 }
