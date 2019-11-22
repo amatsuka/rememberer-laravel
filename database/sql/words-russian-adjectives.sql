5414c7dcdbcd5344c73df1d217e9b9e79ce1c2ef
amatsuka <run100500@gmail.com>
2019-11-22 14:39:59 +0300

    Фикс файлов сидов

--- database/sql/words-russian-adjectives.sql
+++ database/sql/words-russian-adjectives.sql
@@ -21,7 +21,7 @@
 
 DROP TABLE IF EXISTS adjectives;
 CREATE TABLE IF NOT EXISTS adjectives (
-  IID INTEGER PRIMARY KEY AUTOINCREMENT,
+  IID int(11) NOT NULL,
   word varchar(60) NOT NULL,
   code int(11) NOT NULL,
   type_sub varchar(60) DEFAULT NULL
